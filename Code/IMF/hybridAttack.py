181dc4af434aa2a562fe716d7a7425daabe85e5c
Rajjat Dadwal <Rajjat@users.noreply.github.com>
2021-10-20 18:52:02 +0200

    Update hybridAttack.py

--- Code/IMF/hybridAttack.py
+++ Code/IMF/hybridAttack.py
@@ -207,7 +207,7 @@ if __name__ == "__main__":
     config.read(args.configfile)
     start = time.time()
     df1 = pd.read_csv(
-        '/data/dadwal/watermarkingTraj/data/All_results/'+config['global']['technique']+'/256_len/watermark_corrWithDistance_finalused.csv',header=None)
+        '/data/watermarkingTraj/data/All_results/'+config['global']['technique']+'/256_len/watermark_corrWithDistance_finalused.csv',header=None)
 
     df1.columns=['trip_id','mean_dist','min_dist','max_dist','watermark_corr']
     trip_idSeries = df1['trip_id'].values
